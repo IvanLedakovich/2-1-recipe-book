44b110b1bd76b1f1ea89eccdd799fff59992f47d
HUBSlover <ivanled2002@gmail.com>
2024-07-26 22:15:48 +0200

    single page visuals partially done

--- src/Pages/RecipePage.tsx
+++ src/Pages/RecipePage.tsx
@@ -2,15 +2,20 @@ import clsx from 'clsx';
 import React from 'react';
 import {
 	bowlContainer,
-	bowlReflectionBig,
-	bowlReflectionSmall,
 	goBackButton,
-	headerLineLeft,
-	headerLineRight,
-	headerLogoContainer
+	headerLineLeftSingle,
+	headerLineRightSingle,
+	headerLogoContainerSingle,
+	headerTextSingle,
+	infoContainerSinglePage,
+	infoContanerSingle,
+	photoContainerSinglePage,
+	recipeImageSinglePage,
+	recipeNameSingle
 } from '../styles/styles';
 
-import bowl from '../images/bowl.svg';
+import bowl from '../images/bowlBlack.svg';
+import RecipeTagsSinglePage from '../RecipiesContainer/RecipeCard/RecipeCardTagsSinglePage';
 
 const RecipePage: React.FC<{ id }> = (id) => {
 	return (
@@ -20,29 +25,34 @@ const RecipePage: React.FC<{ id }> = (id) => {
 					'mt-[50px]',
 					'w-[100%]',
 					'h-[100px]',
-					'bg-red-600',
 					'grid',
 					'items-center'
 				)}
 			>
 				<button className={clsx(goBackButton, 'scale-1')}>Go back</button>
 
-				<div id="headerLine" className={headerLineLeft} />
-				<div id="headerLine" className={headerLineRight} />
-				<div className={headerLogoContainer}>
+				<div id="headerLine" className={headerLineLeftSingle} />
+				<div id="headerLine" className={headerLineRightSingle} />
+				<div className={headerLogoContainerSingle}>
 					<div className={bowlContainer}>
 						<img className={bowl} src={bowl} alt="bowl" />
-						<img
-							className={bowlReflectionBig}
-							src={bowlReflectionBig}
-							alt="bowlReflectionBig"
-						/>
-						<img
-							className={bowlReflectionSmall}
-							src={bowlReflectionSmall}
-							alt="bowlReflectionSmall"
-						/>
 					</div>
+					<h2 className={headerTextSingle}>Recipe Book</h2>
+				</div>
+			</div>
+			<div className={infoContanerSingle}>
+				<div className={photoContainerSinglePage}>
+					<img
+						className={recipeImageSinglePage}
+						src="https://cdn.dummyjson.com/recipe-images/1.webp"
+						alt="bowl"
+					/>
+				</div>
+				<div className={infoContainerSinglePage}>
+					<RecipeTagsSinglePage tags={['italian', 'pizza']} />
+					<h1 className={clsx(recipeNameSingle, 'text-6xl')}>
+						Classic Margherita Pizza
+					</h1>
 				</div>
 			</div>
 		</>
